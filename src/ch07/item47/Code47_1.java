b9376de7b61c2dab9ed52daad747fc18eef3102e
whdms705 <whdms705@nate.com>
2019-05-12 04:13:58 +0900

    Update item47

--- src/ch07/item47/Code47_1.java
+++ src/ch07/item47/Code47_1.java
@@ -2,6 +2,7 @@ package ch07.item47;
 
 
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static java.lang.ProcessHandle.allProcesses;
 
@@ -21,8 +22,13 @@ public class Code47_1 {
         }
 
     }
-
+    // Stream<E>를 Iterable<E>로 중개해주는 어댑터
     public static <E> Iterable<E> iterableOf(Stream<E> stream){
         return stream::iterator;
+    }
+
+    // Iterator<E>를 Stream<E>로 중개해주는 어댑터
+    public static<E> Stream<E> streamOf(Iterable<E> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
     }
 }
